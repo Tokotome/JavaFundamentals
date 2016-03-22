27027e0b1efb23caa85fa26f73cd0402ff4996a2
Tokotome <mandzhukov.bg@gmail.com>
2016-03-22 10:42:30 +0200

    My first commit

---
+++ src/AssignVariables.java
@@ -0,0 +1,14 @@
+public class AssignVariables {
+public static void main(String[]args){
+
+	boolean a = false;
+	String b = "Paolo Alto,CA";
+	char c = 'c';
+	byte d = 127;
+	short e = 32767;
+	float f = 0.5f;
+	double g = 0.1234567891011;
+	long h = 2000000000;
+	
+}
+}
---
+++ src/SumFromOneToN.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumFromOneToN {
+public static void main (String[] args){
+	Scanner console = new Scanner(System.in);
+	
+	int number = Integer.parseInt(console.nextLine());
+	int sum = 0;
+	int count = 1;
+	while (count<=number){
+	sum+=count;
+	count++;
+	}
+	System.out.print("The sum is "+ sum);
+	}
+}
